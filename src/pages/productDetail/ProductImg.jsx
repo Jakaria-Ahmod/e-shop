8c60ad055c349182aad8a0e06425f79a600490e6
md jakria ahmod <jakariaahmodmd@gmail.com>
2025-07-02 16:34:22 +0600

    zoom error

--- src/pages/productDetail/ProductImg.jsx
+++ src/pages/productDetail/ProductImg.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import ReactImageMagnify from 'react-image-magnify';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
+
 const ProductImg = ({ image }) => {
   return (
     <div>
       <div>
-        <div className="w-[833px] h-[541px] rounded-[25px] cursor-pointer border border-gray-400">
-          {/* <img src={image} alt="img" className="w-full h-full object-cover" /> */}
-          <div className="relative w-full h-full overflow-visible rounded-2xl flex items-center justify-center">
-            <ReactImageMagnify
-              {...{
-                smallImage: {
-                  alt: 'Wristwatch by Ted Baker London',
-                  isFluidWidth: false,
-                  src: image,
-                  width: 450,
-                  height: 450,
-                },
-                largeImage: {
-                  src: image,
-                  width: 1200,
-                  height: 1800,
-                },
-                enlargedImageContainerStyle: {
-                  zIndex: 999,
-                  background: '#fff',
-                },
-                isHintEnabled: true,
-                enlargedImagePosition: 'beside',
-              }}
-            />
-          </div>
+        <div className="w-[833px] h-[541px] rounded-[25px] cursor-pointer border border-gray-400 flex items-center justify-center">
+          <Zoom>
+            <img alt="Zoom" src={image} width="500" className="rounded-lg" />
+          </Zoom>
         </div>
         <div className="flex mt-[32px] gap-x-[32px]">
           <div className="w-[94px] h-[82px] bg-black rounded-md overflow-hidden border border-gray-400">
